dce2d9e44cc65c71f76ebc0b051c1f56b8ce6720
millena borriero <129234570+millenaeandre@users.noreply.github.com>
2024-08-30 11:31:50 -0300

    script.js

--- script.js
+++ script.js
@@ -19,54 +19,54 @@ const perguntas = [
         ]
     },
     {
-        enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial, uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
+        enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial, um dos alunos dos seus alunos utilizou o chatgpt para fazer uma redação e você descobriu, qual sua opinião?",
         alternativas: [
             {
-                texto: "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
-                afirmacao: "Conseguiu utilizar a IA para buscar informações úteis."
+                texto: "Deixa utilizar a ferramenta, pois ajuda os alunos a fazerem os trabalhos melhores.",
+                afirmacao: "Deixa utilizar a IA para buscar informações úteis."
             },
             {
-                texto: "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
-                afirmacao: "Sentiu mais facilidade em utilizar seus próprios recursos para escrever seu trabalho."
+                texto: "Concientiza os alunos de que podem ficar dependentes e que não podem usar em certos casos.",
+                afirmacao: "Concientiza em utilizar seus próprios recursos para escrever seu trabalho."
             }
         ]
     },
     {
-        enunciado: "Após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+        enunciado: "Você resolveu realizar uma conversa com a sua turma para saber quem utiliza e o porque eles utilizam. Nessa conversa a maioria dos alunos querem começar a utilizar, como você se posiciona?",
         alternativas: [
             {
-                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
-                afirmacao: "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA."
+                texto: "Expõe a ideia de que a IA pode ajudar a melhorar seus conhecimentos.",
+                afirmacao: "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos com IA."
             },
             {
-                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
-                afirmacao: "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética."
+                texto: "Me preocupo que  vocês perderão oportunidades de aprenderem novas coisas por querer pesquisar e ter tudo nas maõs.",
+                afirmacao: "Preocupação com seus alunos."
             }
         ]
     },
     {
-        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
+        enunciado: "Ao final da discussão, você deixou que usasse a inteligencia artificial para criarem um site no computador que representasse o que pensa sobre IA. E agora?",
         alternativas: [
             {
-                texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
-                afirmacao: "Notou também que muitas pessoas não sabem ainda utilizar as ferramentas tradicionais e decidiu compartilhar seus conhecimentos de design utilizando ferramentas de pintura digital para iniciantes."
+                texto: "Criarem tudo com Ia.",
+                afirmacao: "Notou também que muito de seus alunos não sabem utilizar."
             },
             {
-                texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
-                afirmacao: "Acelerou o processo de criação de trabalhos utilizando geradores de imagem e agora consegue ensinar pessoas que sentem dificuldades em desenhar manualmente como utilizar também!"
+                texto: "Criarem no chatgpt, mas mudarem conforme seus desejos.",
+                afirmacao: "Melhorando a qualidade de seu trabalho"
             }
         ]
     },
     {
-        enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz? ",
+        enunciado: "O que se acha de usar ajuda da IA para trabalhos da escola? ",
         alternativas: [
             {
-                texto: "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
-                afirmacao: "Infelizmente passou a utilizar a IA para fazer todas suas tarefas e agora se sente dependente da IA para tudo."
+                texto: "Fazer trabalhos com a ajuda da IA contribui com o andamento e a agilidade com que eu faço.",
+                afirmacao: " Passou a utilizar a IA para fazer todas suas tarefas e agora se sente dependente da IA para tudo, não conseguindo fazer nada sem."
             },
             {
-                texto: "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
-                afirmacao: "Percebeu que toda IA reproduz orientações baseadas na empresa que programou e muito do que o chat escrevia não refletia o que pensava e por isso sabe que os textos gerados pela IA devem servir como auxílio e não resultado final. "
+                texto: "A IA pode prejudicar, pois hoje em dia existem aplicativos que indentificam se usou o chatgpt ou algo do tipo, prefiro não usar.",
+                afirmacao: "Sabe que é prejudicial. "
             }
         ]
     },
@@ -111,3 +111,4 @@ function mostraResultado() {
 }
 
 mostraPergunta();
+
